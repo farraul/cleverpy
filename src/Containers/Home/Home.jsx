e303fb17820687c6fcfa743497c74706b9af51c8
raul farl <raulfarllobell@gmail.com>
2022-01-27 14:59:30 +0100

    change the padding each post and add the button delete of the post

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,12 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Home.css';
+
 
 
 const Home = () => {
 
-    return (
-        <div className='home-container'>
+    const [allPosts, setAllPosts] = useState([]);
+    let res;
+    useEffect(() => {
+        takePosts();
+    }, []);
 
-            home prueba
+    const takePosts = async () => {
+        try {
+            res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+            setAllPosts(res.data);
+            console.log("res;", res.data);
+        }
+        catch (error) {
+        }
+    }
+
+
+
+    return (
+        <div className='max-width-container-1200 home-container'>
+
+            <div className='home-section-all-posts'>
+
+                {allPosts.map(run =>
+
+                    <div className='home-section-all-posts-each-post'>
+                        <div className='padding-each-post'>
+                            <div>
+                                <strong className='uppercase'>{run.title}</strong>
+                            </div>
+                            <div>
+                                {run.userId}
+                            </div>
+                            <div>
+                                {run.body}
+                            </div>
+                            {console.log("run", run)}
+                            <div>
+                            </div>
+                        </div>
+                        <div className='padding-each-post-delete'>
+                        Eliminar
+                        </div>
+
+                    </div>
+
+                )}
+            </div>
         </div>
 
     )
