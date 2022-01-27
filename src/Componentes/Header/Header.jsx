a6b358a758d7f9a5f7c2afdd1a32ad34d0ee2a61
raul farl <raulfarllobell@gmail.com>
2022-01-27 22:00:40 +0100

    create the link posts in the header

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './Header.css';
 import logo from '../../assets/images/Logotipo.png';
+import { useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
+
+    const history = useNavigate();
+
+    const goToContainer = (url) => {
+        history(url);
+    }
 
     return (
         <header>
 
             <div className='header-section'>
-                <div>
-                    <img className="logo" src={logo}/>
+                <div onClick={() => goToContainer("/")}>
+                    <img className="logo" src={logo} />
                 </div>
-                <p>Header</p>
+                <div className='header-menu'>
+                
+                    <div className="header-menu-each-div" onClick={() => goToContainer("/")}>Posts</div>
+                </div>
             </div>
         </header>
     )
