e8d6547866e21721655c9466367cc1cce8b3f734
raul farl <raulfarllobell@gmail.com>
2022-01-27 13:25:35 +0100

    install react router dom and create the file header and header.js

---
+++ src/Componentes/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Header = () => {
+
+    return (
+        <header>
+
+            <div className='header-section'>
+                <p>Header</p>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
