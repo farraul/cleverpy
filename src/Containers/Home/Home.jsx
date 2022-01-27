4ee15ae497e5e0a903f515334a25fe0c0d8df600
raul farl <raulfarllobell@gmail.com>
2022-01-27 17:31:52 +0100

    in the container home, now we can delete the post with the pop-up

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -7,7 +7,12 @@ import './Home.css';
 const Home = () => {
 
     const [allPosts, setAllPosts] = useState([]);
+    const [postSelected, setpostSelected] = useState([""]);
     let res;
+    let element_back = document.getElementById("pop-up-background-dark-full-width");
+    let elementSeePopUp = document.getElementById("pop-up-black-div-info");
+
+
     useEffect(() => {
         takePosts();
     }, []);
@@ -16,22 +21,39 @@ const Home = () => {
         try {
             res = await axios.get("https://jsonplaceholder.typicode.com/posts");
             setAllPosts(res.data);
-            console.log("res;", res.data);
         }
         catch (error) {
         }
     }
 
 
+    const seePopUp = (id_post) => {
+        setpostSelected(id_post);
+        elementSeePopUp.classList.add("block-sreen-pop-up");
+        element_back.classList.add("see-background-full-dark");
+    }
+
+    const hidePopUp = async () => {
+        elementSeePopUp.classList.remove("block-sreenpop-up");
+        element_back.classList.remove("see-background-full-dark");
+    }
+
+
+    const deletePost = async () => {
+        setAllPosts(allPosts.filter(function (post) {
+            console.log("post.id",post.id)
+            return post.id != postSelected
+        }
+        ))
+        console.log("eliminado:", allPosts)
+    }
 
     return (
         <div className='max-width-container-1200 home-container'>
-
             <div className='home-section-all-posts'>
-
                 {allPosts.map(run =>
 
-                    <div className='home-section-all-posts-each-post'>
+                    <div key={run.id} className='home-section-all-posts-each-post'>
                         <div className='padding-each-post'>
                             <div>
                                 <strong className='uppercase'>{run.title}</strong>
@@ -42,17 +64,38 @@ const Home = () => {
                             <div>
                                 {run.body}
                             </div>
-                            {console.log("run", run)}
+
                             <div>
                             </div>
                         </div>
-                        <div className='padding-each-post-delete'>
-                        Eliminar
+
+                        <div onClick={() => seePopUp(run.id)} className='padding-each-post-delete'>
+                            Eliminar {run.id}
+                        </div>
+                    </div>
+                )}
+            </div>
+
+
+
+            <div id="pop-up-background-dark-full-width">
+                <div className="pop-up-div-info" id="pop-up-black-div-info">
+                    <div className="pop-up-div-info-title-cross">
+                        <div className="closeWindow" id="X" onClick={() => hidePopUp()}>X</div>
+                        <p className='pop-up-div-info-title'>¿Estas seguro de borrar este post?</p>
+                    </div>
+                    <div className='buttons-pop-up'>
+                        <div onClick={() => hidePopUp()} className='button-no-delete'>
+                            No
+                        </div>
+                        <div onClick={() => deletePost()} className='button-delete'>
+                            Sí
                         </div>
 
                     </div>
-
-                )}
+                    <div className='iframe-arquitects-pop-up'>
+                    </div>
+                </div>
             </div>
         </div>
 
